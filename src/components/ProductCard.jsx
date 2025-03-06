f57b2946795d4135243cd0d109483f4b26d211e6
Marcosgit <marcosgervacio7@gmail.com>
2025-03-06 19:43:28 -0300

    render stock principal

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,9 +3,25 @@ import {Link} from 'react-router-dom'
 
 export default function ProductCard({product}){
 
+    const sinStock = {
+        backgroundColor: "rgba(255, 95, 95, 0.5)",
+        width: "90px",
+        margin: "0 auto",
+        border: "1px solid black", 
+        borderRadius: "10px",
+    }
+    const enStock = {
+        backgroundColor: "rgba(102, 247, 102, 0.5)",
+        width: "90px",
+        margin: "0 auto",
+        border: "1px solid black", 
+        borderRadius: "10px",
+    }
+
     return(
         <>
             <article className='articleProduct'>
+            {product.stock === 0 ? <p style={sinStock}>Sin stock</p> : <p style={enStock}>En stock</p>}
                 <h3>
                     {product.title}   
                 </h3>    
